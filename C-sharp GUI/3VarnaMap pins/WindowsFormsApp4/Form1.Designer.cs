22896b5f93a1fe8d134614b54b6bd743880db89a
nikiyyy <44340403+nikiyyy@users.noreply.github.com>
2020-02-02 13:55:47 +0200

    Add files via upload

---
+++ C-sharp GUI/3VarnaMap pins/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.endToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.скеийОбектToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.преместиОбектToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сприПреместванетоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отстарниОбектToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.p0 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.hideToolStripMenuItem,
+            this.endToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(131, 92);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.newToolStripMenuItem.Text = "Нов обект";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.viewToolStripMenuItem.Text = "Покажи";
+            this.viewToolStripMenuItem.Click += new System.EventHandler(this.ViewToolStripMenuItem_Click);
+            // 
+            // hideToolStripMenuItem
+            // 
+            this.hideToolStripMenuItem.Name = "hideToolStripMenuItem";
+            this.hideToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.hideToolStripMenuItem.Text = "Скрий";
+            this.hideToolStripMenuItem.Click += new System.EventHandler(this.HideToolStripMenuItem_Click);
+            // 
+            // endToolStripMenuItem
+            // 
+            this.endToolStripMenuItem.Name = "endToolStripMenuItem";
+            this.endToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.endToolStripMenuItem.Text = "Край";
+            this.endToolStripMenuItem.Click += new System.EventHandler(this.EndToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.скеийОбектToolStripMenuItem,
+            this.преместиОбектToolStripMenuItem,
+            this.сприПреместванетоToolStripMenuItem,
+            this.отстарниОбектToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(190, 92);
+            this.contextMenuStrip2.Opening += new System.ComponentModel.CancelEventHandler(this.ContextMenuStrip2_Opening);
+            // 
+            // скеийОбектToolStripMenuItem
+            // 
+            this.скеийОбектToolStripMenuItem.Name = "скеийОбектToolStripMenuItem";
+            this.скеийОбектToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.скеийОбектToolStripMenuItem.Text = "Скрий Обект";
+            this.скеийОбектToolStripMenuItem.Click += new System.EventHandler(this.hideToolStripMenuItem_Click);
+            // 
+            // преместиОбектToolStripMenuItem
+            // 
+            this.преместиОбектToolStripMenuItem.Name = "преместиОбектToolStripMenuItem";
+            this.преместиОбектToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.преместиОбектToolStripMenuItem.Text = "Примести Обект";
+            this.преместиОбектToolStripMenuItem.Click += new System.EventHandler(this.move);
+            // 
+            // сприПреместванетоToolStripMenuItem
+            // 
+            this.сприПреместванетоToolStripMenuItem.Name = "сприПреместванетоToolStripMenuItem";
+            this.сприПреместванетоToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.сприПреместванетоToolStripMenuItem.Text = "Спри преместването";
+            this.сприПреместванетоToolStripMenuItem.Click += new System.EventHandler(this.stop);
+            // 
+            // отстарниОбектToolStripMenuItem
+            // 
+            this.отстарниОбектToolStripMenuItem.Name = "отстарниОбектToolStripMenuItem";
+            this.отстарниОбектToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.отстарниОбектToolStripMenuItem.Text = "Отстарни обект";
+            this.отстарниОбектToolStripMenuItem.Click += new System.EventHandler(this.removeobj);
+            // 
+            // p0
+            // 
+            this.p0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.p0.ContextMenuStrip = this.contextMenuStrip2;
+            this.p0.Image = global::WindowsFormsApp4.Properties.Resources.flag_64x64;
+            this.p0.Location = new System.Drawing.Point(57, 76);
+            this.p0.Name = "p0";
+            this.p0.Size = new System.Drawing.Size(22, 20);
+            this.p0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p0.TabIndex = 1;
+            this.p0.TabStop = false;
+            this.p0.Visible = false;
+            this.p0.MouseDown += new System.Windows.Forms.MouseEventHandler(this.P0_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.Map_of_Varna;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(670, 392);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picmapmmove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 392);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.p0);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.p0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox p0;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem endToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem скеийОбектToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem преместиОбектToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сприПреместванетоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отстарниОбектToolStripMenuItem;
+    }
+}
+
---
+++ C-sharp GUI/3VarnaMap pins/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+
+        public List<PictureBox> p = new List<PictureBox>();
+        PictureBox pp = new PictureBox();
+        public object sender1 = new object();
+        public string senderName;
+        Boolean parMove;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        { 
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+
+            PictureBox NewP = new PictureBox();
+            NewP.Name = "p" + (p.Count + 1).ToString();
+            NewP.Width = p0.Width;
+            NewP.Height = p0.Height;
+
+            NewP.Image = p0.Image;
+            NewP.SizeMode = p0.SizeMode;
+            
+            NewP.ContextMenu = p0.ContextMenu;
+            NewP.ContextMenuStrip = this.contextMenuStrip2;
+            NewP.Visible = true;
+            NewP.Left = Cursor.Position.X -23;
+            NewP.Top = Cursor.Position.Y -25;
+            
+            NewP.Click += new System.EventHandler(this.hideToolStripMenuItem_Click);
+            NewP.MouseDown += new System.Windows.Forms.MouseEventHandler(this.P0_MouseDown);
+            
+            p.Add(NewP);
+            this.Controls.Add(NewP);
+            pictureBox1.SendToBack();
+            
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (PictureBox pp in p)
+            {
+                pp.Visible = true;
+                pp.Enabled = true;// деблокира обекти, ако има такива
+            }
+        }
+
+        private void HideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (PictureBox pp in p)
+            {
+                pp.Visible = false;
+                pp.Enabled = true;
+            }
+        }
+
+        private void EndToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void P0_MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    // Left click
+                    break;
+                case MouseButtons.Right:
+                    senderName = ((PictureBox)sender).Name;
+                    break;
+            }
+
+        }
+
+        private void ContextMenuStrip2_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void hideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (PictureBox pp in p)
+            {
+                if ((senderName == pp.Name))
+                {
+                    pp.Visible = false;
+                    pp.Tag = 0;
+                }
+            }
+        }
+
+        private void move(object sender, EventArgs e)
+        {
+            parMove = true;
+            
+        }
+        private void removeobj(object sender, EventArgs e)
+        {
+            foreach (PictureBox pp in p)
+            {
+                if ((senderName == pp.Name))
+                {
+                    pp.Visible = false;
+                    p.Remove(pp);
+                    break;
+
+                }
+            }
+        }
+
+        private void picmapmmove(object sender, MouseEventArgs e)
+        {
+            if (parMove == true) //Cursor.Position.X
+            {
+                foreach (PictureBox pp in p)
+                {
+                    if (senderName == pp.Name)
+                    {
+                        pp.Left = ((e.X + (pp.Width / 2) - (pp.Cursor.Size.Width / 2)));
+                        pp.Top = ((e.Y + (pp.Height / 2) - (pp.Cursor.Size.Height / 2)));
+                    }
+                }
+
+            }
+        }
+
+        private void stop(object sender, EventArgs e)
+        {
+            parMove = false;
+        }
+    }
+}
